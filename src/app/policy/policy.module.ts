d6049876061d8210b52b5504680dedd455c87f73
程守一 <chengshouyi@skywo-safe.com>
2022-11-24 14:18:51 +0800

    树形图示例

--- src/app/policy/policy.module.ts
+++ src/app/policy/policy.module.ts
@@ -5,16 +5,20 @@ import {PolicyRoutingModule} from './policy-routing.module';
 import {PolicyComponent} from './policy.component';
 import {SharedModule} from "../shared/shared.module";
 import {DragDropModule} from '@angular/cdk/drag-drop';
+import {TreeComponent} from "./tree/tree.component";
+import {NgxGraphModule} from "@swimlane/ngx-graph";
 
 @NgModule({
   declarations: [
-    PolicyComponent
+    PolicyComponent,
+    TreeComponent
   ],
   imports: [
     CommonModule,
     PolicyRoutingModule,
     SharedModule,
-    DragDropModule
+    DragDropModule,
+    NgxGraphModule
   ]
 })
 export class PolicyModule {
