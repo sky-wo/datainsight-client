01f5e18334df5d503014ee9768bc63c929b3de91
程守一 <chengshouyi@skywo-safe.com>
2022-11-25 09:07:53 +0800

    暂存

--- src/app/policy/tree/tree.component.ts
+++ src/app/policy/tree/tree.component.ts
@@ -2,70 +2,50 @@ import {Component, OnInit} from '@angular/core';
 import {DagreNodesOnlyLayout, Edge, Layout, Node} from "@swimlane/ngx-graph";
 
 import * as shape from 'd3-shape';
+import {DataTag} from "../../core/type/graphql-type";
+import {PolicyService} from "../../core/service/policy.service";
+import {NzNotificationService} from "ng-zorro-antd/notification";
 
 @Component({
-  selector: 'app-tree',
-  templateUrl: './tree.component.html',
-  styleUrls: ['./tree.component.less']
+  selector: 'app-tree', templateUrl: './tree.component.html', styleUrls: ['./tree.component.less']
 })
 export class TreeComponent implements OnInit {
 
+  originalData: DataTag[] = []
+  originalDataCopy: DataTag[] = []
+  ww: DataTag | null = null;
+
   public layout: Layout = new DagreNodesOnlyLayout();
-  public curve: any = shape.curveBasis;
+  public curve: any = shape.curveBasis
+  public links: Edge[] = []
+  public nodes: Node[] = []
 
-  public links: Edge[] = [
-    {
-      id: 'a',
-      source: 'first',
-      target: 'second'
-    },
-    {
-      id: 'b',
-      source: 'first',
-      target: 'third'
-    },
-    {
-      id: 'c',
-      source: 'first',
-      target: 'fourth'
-    },
-    {
-      id: 'c95',
-      source: 'r2',
-      target: 'c2'
-    }
-  ];
-  public nodes: Node[] = [
-    {
-      id: 'first',
-      label: 'wwa'
-    },
-    {
-      id: 'second',
-      label: 'ww'
-    },
-    {
-      id: 'third',
-      label: 'ww'
-    },
-    {
-      id: 'fourth',
-      label: 'ww'
-    },
-    {
-      id: 'r2',
-      label: 'wwdd'
-    },
-    {
-      id: 'c2',
-      label: 'ww'
-    }
-  ];
-
-  constructor() {
+  constructor(private policyService: PolicyService, private notification: NzNotificationService) {
   }
 
   ngOnInit(): void {
+    this.loadAllPolicys()
+  }
+
+  loadAllPolicys() {
+    this.policyService.pagingQueryDataTags(200, 0).valueChanges.subscribe({
+      next: r => {
+        this.originalData = r.data.dataTags.items as DataTag[]
+        for (let i = 0; i < this.originalData.length; i++) {
+          // 深拷贝 解决属性不可扩展的错误
+          this.originalDataCopy.push(JSON.parse(JSON.stringify(this.originalData[i])))
+          let myObj = {id: this.originalData[i].id, data: this.originalDataCopy[i], label: this.originalDataCopy[i].name}
+          this.nodes.push(myObj)
+          if (this.originalData[i].parentId) {
+            let myObj = {source: this.originalData[i].parentId!, target: this.originalData[i].id}
+            this.links.push(myObj)
+          }
+        }
+      }, error: e => {
+        console.error(e)
+      }
+    })
   }
 
 }
+
